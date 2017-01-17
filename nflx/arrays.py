da188e2d9075bd72913b91bfd2da1749129d877d
fxfitz <francis.x.fitzpatrick@gmail.com>
2017-01-16 20:10:19 -0600

    sorts the output

--- nflx/arrays.py
+++ nflx/arrays.py
@@ -2,4 +2,4 @@ def diff(current, target):
     additions = [x for x in target if x not in current]
     subtractions = [x for x in current if x not in target]
 
-    return additions, subtractions
+    return sorted(additions), sorted(subtractions)
