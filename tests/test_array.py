d1ed7a4f0fa6097351757f87df6b96ade893233c
fxfitz <francis.x.fitzpatrick@gmail.com>
2017-01-16 20:10:04 -0600

    Adds additional test cases

--- tests/test_array.py
+++ tests/test_array.py
@@ -4,7 +4,10 @@ import nflx.arrays
 
 
 @pytest.mark.parametrize('current,target,expected', [
-    ([1, 3, 5, 6, 8, 9], [1, 2, 5, 7, 9], ([2, 7], [3, 6, 8]))
+    ([1, 3, 5, 6, 8, 9], [1, 2, 5, 7, 9], ([2, 7], [3, 6, 8])),  # from sample
+    ([], [1, 2, 3, 4, 5], ([1, 2, 3, 4, 5], [])),  # test only additions
+    ([1, 2, 3, 4, 5], [], ([], [1, 2, 3, 4, 5])),  # test only subtractions
+    ([3, 1, 2], [5, 2], ([5], [1, 3])),  # test ordering
 ])
 def test_array_diff(current, target, expected):
     assert nflx.arrays.diff(current, target) == expected
